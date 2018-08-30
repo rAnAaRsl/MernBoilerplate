b9b768e0cbb9c8b00254a91e0043316b543c5a4f
rAnAaRsL <awesum.rana@gmail.com>
2018-08-30 15:33:23 +0500

    Create Profile Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import {setCurrentUser, logoutUser} from "./actions/authAction";
+import {clearCurrentProfile} from "./actions/profileActions";
 import store from './store';
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -11,7 +12,9 @@ import Footer from './components/layout/footer';
 import Landing from './components/layout/landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+import PrivateRoute from './components/common/PrivateRoute';
 
 // Check for Token
 
@@ -37,7 +40,12 @@ if(localStorage.jwtToken)
 
         store.dispatch(logoutUser());
 
-        // TODO: Clear current Profile
+
+        // Clear Profile
+
+        store.dispatch(clearCurrentProfile());
+
+
         // Redirect to login
 
         window.locale.href ='/login';
@@ -55,6 +63,12 @@ class App extends Component {
                         <div className={"container"}>
                             <Route exact path={"/register"} component={Register}/>
                             <Route exact path={"/login"} component={Login}/>
+                            <Switch>
+                            <PrivateRoute exact path={"/dashboard"} component={Dashboard}/>
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute exact path={"/create-profile"} component={CreateProfile}/>
+                            </Switch>
                         </div>
                         <Footer/>
                     </div>
---
+++ client/src/components/common/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import spinner from './spinner.gif';
+
+export default ()=>{
+    return(
+        <di>
+            <img src={spinner} style={{width:'200px',margin:'auto',display:'block'}} alt={"Loading..."}></img>
+        </di>
+    );
+}
